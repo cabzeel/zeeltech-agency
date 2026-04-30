e9e764f9aa54081e949ed3719bdbb86318b9acc6
cabzeel <timchiacabzeel797@gmail.com>
2026-04-30 08:17:22 +0100

    feat: further refinement squared" git push

--- backend/server.js
+++ backend/server.js
@@ -53,3 +53,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT} [${process.env.NODE_ENV || 'development'}]`));
+
+module.exports = app;
